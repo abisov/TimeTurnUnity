c85de1605a9577d5518669ec3d4a195d042cfb61
tast3blood <44275513+tast3blood@users.noreply.github.com>
2020-07-14 23:48:50 +0300

    gg

---
+++ Assets/Scripts/Time/TimeStateManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TimeState { past, present, future}
+public class TimeStateManager : MonoBehaviour
+{
+    TimeState timeState = TimeState.present;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
