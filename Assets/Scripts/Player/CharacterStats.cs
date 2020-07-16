bf23c45ed598fb288ef8ca7adda8ff875808bcdd
Bobi2000 <bobko_2000@abv.bg>
2020-07-17 01:25:56 +0300

    Adding CharacterStats.cs

---
+++ Assets/Scripts/Player/CharacterStats.cs
@@ -0,0 +1,32 @@
+﻿public class CharacterStats
+{
+    private int type;
+    private int range;
+    private float health;
+    private float damage;
+
+    public int Type
+    {
+        get { return this.type; }
+        set { this.type = value; }
+    }
+
+    public int Range
+    {
+        get { return this.range; }
+        set { this.range = value; }
+
+    }
+
+    public float Health
+    {
+        get { return this.health; }
+        set { this.health = value; }
+    }
+
+    public float Damage
+    {
+        get { return this.damage; }
+        set { this.damage = value; }
+    }
+}
